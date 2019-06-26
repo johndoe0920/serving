8438973f5d49049caff9bb134f1e591a49461c70
Ross Wagner <rowagne@microsoft.com>
2019-06-26 13:10:01 -0700

    Check for config

--- pkg/reconciler/clusteringress/clusteringress.go
+++ pkg/reconciler/clusteringress/clusteringress.go
@@ -202,7 +202,7 @@ func (c *Reconciler) reconcile(ctx context.Context, ci *v1alpha1.ClusterIngress)
 		var host = ci.Spec.Rules[0].Hosts[0]
 		fmt.Printf("Host %s \n", ci.Spec.Rules[0].Hosts[0])
 		var domainName = strings.SplitAfterN(host, ".", 2)
-		fmt.Printf("Host after formatting %s\n", hostarray)
+		fmt.Printf("domainName %s\n", domainName)
 		fmt.Printf("Hosts Type %T\n", ci.Spec.Rules[0].Hosts[0])
 		secret, err := c.secretLister.Secrets("istio-system").Get(ci.Spec.Rules[0].Hosts[0])
 		if err != nil {
