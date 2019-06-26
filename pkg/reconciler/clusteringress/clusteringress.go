57ec49739fc23729c9475c32343ad8ceeda4b9a0
Ross Wagner <rowagne@microsoft.com>
2019-06-26 11:27:18 -0700

    Check for config

--- pkg/reconciler/clusteringress/clusteringress.go
+++ pkg/reconciler/clusteringress/clusteringress.go
@@ -216,7 +216,7 @@ func (c *Reconciler) reconcile(ctx context.Context, ci *v1alpha1.ClusterIngress)
 		}
 
 		fmt.Printf("Hosts Type %T\n", ci.Spec.Rules[0].Hosts[0])
-		secret, err := c.secretLister.Secrets("istio-system").Get(ci.Spec.Rules[0].Hosts[0]
+		secret, err := c.secretLister.Secrets("istio-system").Get(ci.Spec.Rules[0].Hosts[0])
 		fmt.Printf("Host Secrets: ", secret)
 
 		// fmt.Println("ClusterIngress: ", ci)
